26b6f95af0498ddb1897c584c80239322d2f4382
DiegoDuenez <19170154@uttcampus.edu.mx>
2020-11-29 23:22:20 -0600

    Se agrego el item para el recyclerview y se actualizo las opciones de intents

--- app/src/main/java/com/example/proyectoavanze/LoginActivity.java
+++ app/src/main/java/com/example/proyectoavanze/LoginActivity.java
@@ -6,15 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btn_reg_1 =  findViewById(R.id.btn_reg_1);
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.btn_reg_1).setOnClickListener(this);
+
+        /*Button btn_reg_1 =  findViewById(R.id.btn_reg_1);
+        Button btn_login = findViewById(R.id.btn_login);
+
 
 
         btn_reg_1.setOnClickListener(new View.OnClickListener() {
@@ -24,9 +30,31 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "La cuenta no es valida", Toast.LENGTH_LONG);
+            }
+        });*/
+
     }
 
 
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()){
 
+            case R.id.btn_reg_1:
+                Intent intent = new Intent(getApplicationContext(), RegistroActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.btn_login:
+                Toast.makeText(getApplicationContext(), "La cuenta no es valida", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+    }
 }
