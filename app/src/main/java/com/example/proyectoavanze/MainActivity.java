7e86c3761f4cabcae19ce41dc091f4e941b458f1
DiegoDuenez <19170154@uttcampus.edu.mx>
2020-11-30 12:21:57 -0600

    Intent del splash al activity de permisos

--- app/src/main/java/com/example/proyectoavanze/MainActivity.java
+++ app/src/main/java/com/example/proyectoavanze/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.proyectoavanze;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ASK_PERMISSION = 124;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +23,26 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, PermisosActivity.class);
-                startActivity(i);
-                finish();
+                int permisoLlamar = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE);
+                int permisoContactos = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS);
+                if(permisoLlamar != PackageManager.PERMISSION_GRANTED || permisoContactos != PackageManager.PERMISSION_GRANTED){
+                    Intent i = new Intent(MainActivity.this, PermisosActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         }, 2000);
 
 
     }
+
+
+
+
 }
--- app/src/main/java/com/example/proyectoavanze/PermisosActivity.java
+++ app/src/main/java/com/example/proyectoavanze/PermisosActivity.java
@@ -1,23 +1,36 @@
 package com.example.proyectoavanze;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PermisosActivity extends AppCompatActivity {
+public class PermisosActivity extends AppCompatActivity{
+
+
+    private static final int REQUEST_CODE_ASK_PERMISSION = 124;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permisos);
+
+        sw = findViewById(R.id.sw_permiso);
 
         RecyclerView recyclerView = findViewById(R.id.recview);
 
@@ -26,10 +39,9 @@ public class PermisosActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         Toast.makeText(this, "Acepta los permisos para poder continuar", Toast.LENGTH_LONG).show();
-
-
         List<Permiso> permisosList = new ArrayList<>();
         permisosList.add(new Permiso("prueba titulo 1"));
+        permisosList.add(new Permiso("prueba titulo 2"));
 
         AdaptadorPermisos adapter = new AdaptadorPermisos(permisosList);
 
@@ -39,5 +51,19 @@ public class PermisosActivity extends AppCompatActivity {
         adapter.notifyDataSetChanged();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
