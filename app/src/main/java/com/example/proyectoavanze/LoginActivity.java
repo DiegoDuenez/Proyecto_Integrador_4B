ced27aba51dbd8988f3d1d4d3b09fe7082825a2f
DiegoDuenez <19170154@uttcampus.edu.mx>
2020-11-29 22:55:28 -0600

    Se agrego el intent para la opcion de registrarte

--- app/src/main/java/com/example/proyectoavanze/LoginActivity.java
+++ app/src/main/java/com/example/proyectoavanze/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.proyectoavanze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_reg_1 =  findViewById(R.id.btn_reg_1);
+
+
+        btn_reg_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+
 }
